ee8242b1c3b07e99fb226d4d3d000de58661edca
truongvantoan <truongvantoan.neu@gmail.com>
2019-04-22 09:55:12 +0700

    show calendar-react 2

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import './App.css'
 import Calendar from './Components/Calendar'
 
-const style = {
-    position: "relative",
-    margin: "50px auto"
-  }
+
 class App extends Component {
      
     render() {
         return (
             <div className="App">
-                <Calendar style={style} width="302px" />
-            </div>
+            <header>
+              <div id="logo">
+                <span className="icon">date_range</span>
+                <span>
+                  react<b>calendar</b>
+                </span>
+              </div>
+            </header>
+            <main>
+              <Calendar />
+            </main>
+          </div>
         );
     }
 }

bd9f44b97219bfcbaa508e93908581176c19d22b
truongvantoan <truongvantoan.neu@gmail.com>
2019-04-25 10:33:58 +0700

    select-fruit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import './App.css'
-import SetColor from './components/setColor'
-import SetFont from './components/SetFont'
-import Result from './components/Result'
-import Reset from './components/Reset'
+
 class App extends Component {
-
+    constructor(props){
+        super(props)
+        this.state = {
+            fruitName: ''
+        }
+        this.onHandleChange = this.onHandleChange.bind(this)
+        this.onHandleSubmit = this.onHandleSubmit.bind(this)
+    }
+    onHandleChange (e){
+        // let target = e.target
+        // let name = target.name
+        // let value = target.value
+        this.setState({
+            fruitName: e.target.value
+        })
+    }
+    onHandleSubmit(value){
+        value.preventDefault()
+    }
   render() {
+      console.log('A',this.state.fruitName);
+      
     return (
-    
-      <div className="container">
-        <div className="row">
-           <SetColor />
-            <SetFont/>
-        </div>
-
-        <div className="row">
-           <Result />
-            <Reset />
-        </div>
-      </div>
+    <div className="container">
+        <form onSubmit={this.onHandleSubmit}>
+            <label>Trái cây: </label> 
+            <select onChange={ this.onHandleChange } name="fruitName">
+                <option value="grapefruit">Grapefruit</option>
+                <option value="lime">Lime</option>
+                <option selected value="coconut">Coconut</option>
+                <option value="mango">Mango</option>
+            </select>
+            <button type="submit" className="btn btn-danger">Submit</button>
+        </form>
+    </div>
     );
   }
 }

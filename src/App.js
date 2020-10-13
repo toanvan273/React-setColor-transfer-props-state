19a18581a07941ec04044d9d37280644d22ae4ff
Toan Truong Van (Dev - Tech Implementation - Technology - VCCI) <toan.truong@vndirect.com.vn>
2020-10-13 14:50:31 +0700

    up dequy

--- src/App.js
+++ src/App.js
@@ -4,14 +4,56 @@ import SetColor from './components/setColor'
 import SetFont from './components/SetFont'
 import Result from './components/Result'
 import Reset from './components/Reset'
+import Dequy from './view/Dequy'
+const dataSet = [
+    { label: 'Devbe', data: [] },
+    {
+        label: 'HRbe', data: [
+            { label: 'Outside', data: [] },
+            { label: 'Inside', data: [] },
+            {
+                label: 'Beside', data: [
+                    { label: 'conty#', data: [] },
+                    { label: 'trongconty', data: [] }
+                ]
+            },
+        ]
+    },
+    {
+        label: 'Water', data: [
+            { label: 'meme', data: [] },
+            { label: 'haily', data: [] },
+            {
+                label: 'conji', data: [
+                    {
+                        label: '1',
+                        data: [
+                            { label: '1.1', data: [] },
+                            { label: '1.2', data: [] },
+                            { label: '1.3', data: [] },
+                        ]
+                    },
+                    { label: '2', data: [] },
+                    {
+                        label: '3', 
+                        data: [
+                            { label: '3.1', data: [] },
+                            { label: '3.2', data: [] },
+                            { label: '3.3', data: [] },
+                        ]
+                    },
+                ]
+            },
+        ]
+    },
+]
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-
         this.state = {
-            color : 'red',
+            color: 'red',
             fontSize: 16,
-            name:null
+            name: null
         }
     }
     toGetColor = (param) => {
@@ -20,37 +62,39 @@ class App extends Component {
         })
     }
     togetFont = (param) => {
-        if(this.state.fontSize + param >= 8 && this.state.fontSize + param <= 36 ){
+        if (this.state.fontSize + param >= 8 && this.state.fontSize + param <= 36) {
             this.setState({
                 fontSize: this.state.fontSize + param
             })
         }
-        
+
     }
     toReset = (x) => {
-        if(x){
+        if (x) {
             this.setState({
-                color : 'red',
+                color: 'red',
                 fontSize: 16,
             })
         }
     }
-  render() {
-    return (
-    
-      <div className="container">
-        <div className="row">
-           <SetColor color={this.state.color} getColor={this.toGetColor}/>
-            <SetFont fontSize={this.state.fontSize} getFont={this.togetFont}/>
-        </div>
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <SetColor color={this.state.color} getColor={this.toGetColor} />
+                    <SetFont fontSize={this.state.fontSize} getFont={this.togetFont} />
+                </div>
 
-        <div className="row">
-           <Result color={this.state.color} fontSize={this.state.fontSize}/>
-            <Reset toReset={this.toReset}/>
-        </div>
-      </div>
-    );
-  }
+                <div className="row">
+                    <Result color={this.state.color} fontSize={this.state.fontSize} />
+                    <Reset toReset={this.toReset} />
+                </div>
+                <div className="row">
+                    <Dequy data={dataSet} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/view/Dequy.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import MyDequy from './Dequy'
+class Dequy extends React.PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = {
+            dataGet: props.data
+        }
+    }
+    onChoseItem = (item) => {
+        let { dataGet } = this.state
+        console.log("dataGet: ", dataGet);
+        let newData = dataGet
+        newData.map((v, i) => {
+            if (v.label === item.label) {
+                newData.splice(i, 1)
+            }
+        })
+        console.log('2=', dataGet)
+        console.log('newData', newData)
+        this.setState({ dataGet:newData })
+    }
+    render() {
+        let { dataGet } = this.state
+        console.log('render: ', dataGet)
+        return (
+            <React.Fragment >
+                {dataGet.length > 0 &&
+                    dataGet.map((item, index) => {
+                        return (
+                            <div className={'label_' + item.label} key={index} >
+                                <h3 onClick={() => this.onChoseItem(item, index)}>{item.label}</h3>
+                                {item.data && item.data.length > 0 && <MyDequy data={item.data} />}
+                            </div>
+                        )
+                    })
+                }
+            </React.Fragment>
+        )
+    }
+}
+export default Dequy

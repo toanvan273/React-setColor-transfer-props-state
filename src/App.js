319fc41985f9a904320a8a06c69038c93a13dbd5
Toan Truong Van (Dev - Tech Implementation - Technology - VCCI) <toan.truong@vndirect.com.vn>
2020-11-16 13:21:28 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,56 +1,45 @@
 import React, { Component } from 'react';
 import './App.css'
-import SetColor from './components/setColor'
-import SetFont from './components/SetFont'
-import Result from './components/Result'
-import Reset from './components/Reset'
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
 
         this.state = {
-            color : 'red',
+            color: 'red',
             fontSize: 16,
-            name:null
+            name: null
         }
     }
-    toGetColor = (param) => {
-        this.setState({
-            color: param
-        })
+   
+    render() {
+        return (
+            <div className="App">
+                <h2>Using CKEditor 5 build in React</h2>
+                <CKEditor
+                    editor={ClassicEditor}
+                    data="<p>Hello from CKEditor 5!</p>"
+                    onReady={editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log('Editor is ready to use!', editor);
+                    }}
+                    onChange={(event, editor) => {
+                        const data = editor.getData();
+                        console.log({ event, editor, data });
+                    }}
+                    onBlur={(event, editor) => {
+                        console.log('Blur.', editor);
+                    }}
+                    onFocus={(event, editor) => {
+                        console.log('Focus.', editor);
+                    }}
+                />
+            </div>
+        );
     }
-    togetFont = (param) => {
-        if(this.state.fontSize + param >= 8 && this.state.fontSize + param <= 36 ){
-            this.setState({
-                fontSize: this.state.fontSize + param
-            })
-        }
-        
-    }
-    toReset = (x) => {
-        if(x){
-            this.setState({
-                color : 'red',
-                fontSize: 16,
-            })
-        }
-    }
-  render() {
-    return (
-    
-      <div className="container">
-        <div className="row">
-           <SetColor color={this.state.color} getColor={this.toGetColor}/>
-            <SetFont fontSize={this.state.fontSize} getFont={this.togetFont}/>
-        </div>
-
-        <div className="row">
-           <Result color={this.state.color} fontSize={this.state.fontSize}/>
-            <Reset toReset={this.toReset}/>
-        </div>
-      </div>
-    );
-  }
 }
 
 export default App;
